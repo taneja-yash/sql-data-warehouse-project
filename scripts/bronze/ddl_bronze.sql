f8f60a07083196bb982af57fc9da321e2c216ff2
Yash Taneja <62446644+taneja-yash@users.noreply.github.com>
2025-06-18 06:06:04 -0400

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -14,6 +14,8 @@ Run this script to re-define the DDL structure of the 'bronze' tables.
 IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_cust_info;
 
+GO
+
 CREATE TABLE bronze.crm_cust_info (
 	cst_id INT,
 	cst_key NVARCHAR(50),
@@ -24,8 +26,12 @@ CREATE TABLE bronze.crm_cust_info (
 	cst_create_date DATE
 );
 
+GO
+
 IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_prd_info;
+
+GO
 
 CREATE TABLE bronze.crm_prd_info (
 	prd_id INT,
@@ -37,8 +43,12 @@ CREATE TABLE bronze.crm_prd_info (
 	prt_end_dt DATE
 );
 
+GO
+
 IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_sales_details;
+
+GO
 
 CREATE TABLE bronze.crm_sales_details (
 	sls_ord_num NVARCHAR(50),
@@ -52,9 +62,12 @@ CREATE TABLE bronze.crm_sales_details (
 	sls_price INT
 );
 
+GO
 
 IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_cust_az12;
+
+GO
 
 CREATE TABLE bronze.erp_cust_az12 (
 	cid NVARCHAR(50),
@@ -62,16 +75,24 @@ CREATE TABLE bronze.erp_cust_az12 (
 	gen NVARCHAR(50)
 );
 
+GO
+
 IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_loc_a101;
+
+GO
 
 CREATE TABLE bronze.erp_loc_a101 (
 	cid NVARCHAR(50),
 	cntry NVARCHAR(50),
 	);
 
+GO
+
 IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_px_cat_g1v2;
+
+GO
 
 CREATE TABLE bronze.erp_px_cat_g1v2 (
 	id NVARCHAR(50),
