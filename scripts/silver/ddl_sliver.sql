4e376f719e4a011367f04086a987c38962c6d67c
Yash Taneja <62446644+taneja-yash@users.noreply.github.com>
2025-06-18 06:06:58 -0400

    Update ddl_sliver.sql

--- scripts/silver/ddl_sliver.sql
+++ scripts/silver/ddl_sliver.sql
@@ -13,6 +13,8 @@ Run this script to re-define the DDL structure of the 'Silver' tables.
 IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
 	DROP TABLE silver.crm_cust_info;
 
+GO
+
 CREATE TABLE silver.crm_cust_info (
 	cst_id INT,
 	cst_key NVARCHAR(50),
@@ -24,8 +26,12 @@ CREATE TABLE silver.crm_cust_info (
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
+GO
+
 IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
 	DROP TABLE silver.crm_prd_info;
+
+GO
 
 CREATE TABLE silver.crm_prd_info (
 	prd_id INT,
@@ -38,6 +44,8 @@ CREATE TABLE silver.crm_prd_info (
 	prd_end_dt DATE,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
+
+GO
 
 IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
 	DROP TABLE silver.crm_sales_details;
@@ -55,9 +63,12 @@ CREATE TABLE silver.crm_sales_details (
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
+GO
 
 IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
 	DROP TABLE silver.erp_cust_az12;
+
+GO
 
 CREATE TABLE silver.erp_cust_az12 (
 	cid NVARCHAR(50),
@@ -66,8 +77,12 @@ CREATE TABLE silver.erp_cust_az12 (
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
+GO
+
 IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
 	DROP TABLE silver.erp_loc_a101;
+
+GO
 
 CREATE TABLE silver.erp_loc_a101 (
 	cid NVARCHAR(50),
@@ -75,8 +90,12 @@ CREATE TABLE silver.erp_loc_a101 (
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 	);
 
+GO
+
 IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
 	DROP TABLE silver.erp_px_cat_g1v2;
+
+GO
 
 CREATE TABLE silver.erp_px_cat_g1v2 (
 	id NVARCHAR(50),
